803ea0538a09464c4af58342c0b5733c3e34e5e7
vivek rajoriya <vivek.k@daffodilsw.com>
2019-07-20 21:39:39 +0530

    code merge

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/header';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import Entry from './components/entry/entry';
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { HashHistory, BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Reminder from './components/reminder/reminder';
 import reminderReducer from './redux/reducers/reminder_reducer';
 
